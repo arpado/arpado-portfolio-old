8369b2aacd15dcadd8d823046401b1810c8daf00
arpado <ozsvar.arpad@gmail.com>
2022-09-11 15:24:49 +0200

    trying pics

--- components/modals/ModalView.vue
+++ components/modals/ModalView.vue
@@ -106,7 +106,7 @@ export default {
   },
   computed: {
     getPic() {
-      return "/assets/images/" + mainStore.modalContent.image
+      return "/assets/images/" + this.mainStore.modalContent.image
     }
   },
   mounted() {
