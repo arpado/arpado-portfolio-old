58bad24139bd7b58e1bb6952e320e1c0c755b969
arpado <ozsvar.arpad@gmail.com>
2022-09-12 19:50:26 +0200

    cleanup

--- layouts/NoData.vue
+++ layouts/NoData.vue
@@ -19,8 +19,6 @@ export default {};
   left: 0;
   width: 100%;
   height: 100%;
-  /* background: black; */
-  
   display: flex;
   justify-content: center;
   align-items: center;
@@ -34,7 +32,6 @@ export default {};
   position: absolute;
   top: 0;
   left: 0;
-  /* background: black; */
   opacity: 0.6;
 }
 .warning-triangle-container {
