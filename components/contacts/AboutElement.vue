35e28786f7d4d388b56c5dcc9464bbdff9f0ec4f
arpado <ozsvar.arpad@gmail.com>
2022-09-12 21:42:20 +0200

    style and content changes

--- components/contacts/AboutElement.vue
+++ components/contacts/AboutElement.vue
@@ -1,9 +1,10 @@
 <template>
   <div
     class="about-container"
+    ref="aboutContainer"
     :class="{ 'static-effect': mainStore.staticEffectActive }"
     data-augmented-ui="
-  tl-2-clip-y tr-clip r-clip-y b-clip-y bl-clip both
+  tl-2-clip-y tr-clip r-clip-y bl-clip both
 "
   >
     <h3 class="title">About Me:</h3>
@@ -14,22 +15,26 @@
         learn the ropes of coding during the Great Pandemics.
       </p>
       <hr />
-      <h4>How and Why?</h4>
+      <h4>What do I do?</h4>
       <p>
-        I'm proficient with the Vue ecosystem (Vue3, Nuxt3, Pinia, Router), but
+        I'm proficient with the Vue ecosystem (Vue3, Nuxt3, Pinia and Router), but
         as web development relies heavily on plain old JavaScript, I have
-        aquired a deep understanding of HTML-CSS-Javascript too.
+        aquired a deep understanding of HTML-CSS-JavaScript too, plus a little
+        bit of MySQL and I'm also familiar with Git, npm, bash and test-driven-development.
       </p>
       <hr />
-      <h4>What can I do for you?</h4>
+      <h4>How may I help you?</h4>
       <p>
-        In other words I can deliver you anything small, one page websites to
-        complex web applications.
+        <!-- In other words  -->
+        I can deliver anything ranging from a small, one page
+        website to a complex web application, and make your site fully responsive
+        and indexed high on the list of search engines with SEO. When you are
+        satisfied I can even host and maintain your website for you.
       </p>
       <hr />
       <p>
         You can find my resume
-        <a href="assets/pdf/Vue-3-Cheat-Sheet.pdf" target="_blank">HERE</a>.
+        <a href="/pdf/arpad-ozsvar-cv.pdf" target="_blank">HERE</a>.
       </p>
     </div>
   </div>
@@ -49,13 +54,18 @@ export default {
 <style scoped>
 .about-container {
   min-height: 80vh;
-  height: fit-content;
-  width: 80vw;
+  /* height: fit-content; */
+  /* height: 80vh;*/
+  width: 80vw; 
   display: grid;
   grid-template-rows: 1fr 11fr;
   padding: 2rem 3rem;
   --aug-inlay-bg: var(--darkGreen);
   --aug-inlay-opacity: 0.7;
+  --aug-tl-extend1: 30%;
+  --aug-r-extend1: 30%;
+  /* overflow-x: hidden;
+  overflow-y: auto; */
   overflow: hidden;
 }
 .title {
